caec192fc65d23fcb8b6154f5ba2d817e6a7be31
ki1it <kirsh.ru@yandex.ru>
2019-04-14 23:48:22 -0700

    Update sample_chaincode_test.go
    1.The CreateLoanApplication method should take as input: a loan application ID, a JSONstring representing the loan application to create, and the ChaincodeStubInterface, which willbe used to talk to the underlying Hyperledger Fabric infrastructure.
    2.It should return two parameters: the serialized JSON string representing the loan applicationthat was created, and an error object.
    3.It should throw validation error in case of missing/invalid input.

--- sample_chaincode_test.go
+++ sample_chaincode_test.go
@@ -5,3 +5,13 @@ package mainimport
 	"testing"   
 	 "github.com/hyperledger/fabric/core/chaincode/shim"
 )
+func TestCreateLoanApplication (t *testing.T) 
+{    
+	fmt.Println("Entering TestCreateLoanApplication")    
+	attributes := make(map[string][]byte)    //Create a custom MockStub that internally uses shim.MockStub    
+	stub := shim.NewCustomMockStub("mockStub", new(SampleChaincode), attributes)    
+	if stub == nil 
+	{        
+		t.Fatalf("MockStub creation failed")    
+	}
+}
