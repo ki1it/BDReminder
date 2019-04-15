885645d357c80e864dbb728f5f419a52d5c8d283
ki1it <kirsh.ru@yandex.ru>
2019-04-15 00:13:12 -0700

    Update sample_chaincode.go
    Another addition to sample_chaincode.go

--- sample_chaincode.go
+++ sample_chaincode.go
@@ -1,2 +1,14 @@
 package main 
 type SampleChaincode struct {}
+func (t *SampleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) 
+{    
+	return nil, nil
+}
+func (t *SampleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) 
+{    
+	return nil, nil
+}
+func (t *SampleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) 
+{    
+	return nil, nil
+}
