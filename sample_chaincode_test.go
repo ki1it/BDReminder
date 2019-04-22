452d14d2f0330ae0b7c8462b38e81dcf49a40c14
ki1it <kirsh.ru@yandex.ru>
2019-04-22 01:30:41 -0700

    Update sample_chaincode_test.go
    Adding the test for a validation error

--- sample_chaincode_test.go
+++ sample_chaincode_test.go
@@ -14,3 +14,17 @@ func TestCreateLoanApplication (t *testing.T) {
 		t.Fatalf("MockStub creation failed")    
 	}
 }
+func TestCreateLoanApplicationValidation(t *testing.T) {    
+	fmt.Println("Entering TestCreateLoanApplicationValidation")    
+	attributes := make(map[string][]byte)    
+	stub := shim.NewCustomMockStub("mockStub", new(SampleChaincode), attributes)    
+	if stub == nil {        
+		t.Fatalf("MockStub creation failed")    
+	}    
+	stub.MockTransactionStart("t123")    
+	_, err := CreateLoanApplication(stub, []string{})    
+	if err == nil {        
+		t.Fatalf("Expected CreateLoanApplication to return validation error")    
+	}    
+	stub.MockTransactionEnd("t123")
+}
